5d5cd85c92656f850ddbdbfe5cff7c98666262a6
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-01-17 09:43:52 +0900

    UPDATE: refactor

--- works/abs/tasks/arc065_a/arc065a.py
+++ works/abs/tasks/arc065_a/arc065a.py
@@ -8,14 +8,14 @@ def recursive(S):
     d = False
     if S == "":
         return True
-    if len(S) >= 5 and S[0:5] == "dream":
+    if S[0:5] == "dream":
         a = recursive(S[5:])
-        if len(S) >= 7 and S[0:7] == "dreamer":
+        if S[0:7] == "dreamer":
             b = recursive(S[7:])
 
-    if len(S) >= 5 and S[0:5] == "erase":
+    if S[0:5] == "erase":
         c = recursive(S[5:])
-        if len(S) >= 6 and S[0:6] == "eraser":
+        if S[0:6] == "eraser":
             d = recursive(S[6:])
 
     return a or b or c or d
